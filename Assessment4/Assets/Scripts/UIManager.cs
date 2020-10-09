f82af2e13e97b3df950587da4ffba7cc19b2e85a
CB120 <burgio120@gmail.com>
2020-10-09 23:12:33 +1100

    9/10/20
    Just started assignment 4, not really in the mindset tonight to do much but it needed to be started none-the-less

---
+++ Assessment4/Assets/Scripts/UIManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class UIManager : MonoBehaviour
+{
+
+    public void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
