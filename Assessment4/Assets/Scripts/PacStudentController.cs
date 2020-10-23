8fb0ffc72e62175263a6fa78dc4c7464ba047842
CB120 <burgio120@gmail.com>
2020-10-23 15:13:39 +1100

    Made movement Continuous

--- Assessment4/Assets/Scripts/PacStudentController.cs
+++ Assessment4/Assets/Scripts/PacStudentController.cs
@@ -7,9 +7,23 @@ public class PacStudentController : MonoBehaviour
 
     private bool isMoving;
     private Vector3 endPoint;
-    private float moveSpeed = 10f;
-
+    private float moveSpeed = 2f;
+    private KeyCode lastInput;
     public Vector2 TileSize;
+
+
+    private void CheckLastInput() // I wish this would work. :(
+    {
+        if (Input.anyKeyDown)
+        {
+            //lastInput = Event.current.keyCode;*/
+            Debug.Log(lastInput);
+            
+        }
+    } 
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,35 +33,75 @@ public class PacStudentController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        
         if (Input.GetKeyDown(KeyCode.D))
         {
             //isMoving = true;
             endPoint = transform.position + new Vector3(TileSize.x, 0, 0);
             StartCoroutine(Move());
+            lastInput = KeyCode.D;
         }
 
 
         if (Input.GetKeyDown(KeyCode.A))
         {
-            //isMoving = true;
+            
             endPoint = transform.position + new Vector3(-TileSize.x, 0, 0);
             StartCoroutine(Move());
+            lastInput = KeyCode.A;
         }
         
         if (Input.GetKeyDown(KeyCode.W))
         {
-            //isMoving = true;
             endPoint = transform.position + new Vector3(0, TileSize.y, 0);
             StartCoroutine(Move());
+            lastInput = KeyCode.W;
         }
 
         if (Input.GetKeyDown(KeyCode.S))
         {
-            //isMoving = true;
             endPoint = transform.position + new Vector3(0, -TileSize.y, 0);
             StartCoroutine(Move());
+            lastInput = KeyCode.S;
         }
 
+        CheckLastInput();
+
+        if (isMoving == false)
+        {
+            if (lastInput == KeyCode.D){
+                endPoint = transform.position + new Vector3(TileSize.x, 0, 0);
+                StartCoroutine(Move());
+                lastInput = KeyCode.D;
+            } 
+        }
+        
+        {
+            if (lastInput == KeyCode.A)
+            {
+                endPoint = transform.position + new Vector3(-TileSize.x, 0, 0);
+                StartCoroutine(Move());
+                lastInput = KeyCode.A;
+            }
+        }
+       
+        {
+            if (lastInput == KeyCode.W)
+            {
+                endPoint = transform.position + new Vector3(0, TileSize.y, 0);
+                StartCoroutine(Move());
+                lastInput = KeyCode.W;
+            }
+        }
+        
+        {
+            if (lastInput == KeyCode.S)
+            {
+                endPoint = transform.position + new Vector3(0, -TileSize.y, 0);
+                StartCoroutine(Move());
+                lastInput = KeyCode.S;
+            }
+        }
     }
 
     IEnumerator Move()
@@ -61,7 +115,6 @@ public class PacStudentController : MonoBehaviour
         Vector3 startPoint = transform.position;
         while (MoveToPoint(startPoint)) { yield return null; }
         
-
 
         isMoving = false;
     }
