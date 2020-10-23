da55161a3de385a59769b2c1448b053f3b56802a
CB120 <burgio120@gmail.com>
2020-10-23 13:21:51 +1100

    Added pacman movement

--- Assessment4/Assets/Scripts/PacStudentController.cs
+++ Assessment4/Assets/Scripts/PacStudentController.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class PacStudentController : MonoBehaviour
 {
 
+    private bool isMoving;
+    private Vector3 endPoint;
+    private float moveSpeed = 10f;
 
-    
-
-// Start is called before the first frame update
-void Start()
+    public Vector2 TileSize;
+    // Start is called before the first frame update
+    void Start()
     {
         
     }
@@ -17,6 +19,54 @@ void Start()
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //isMoving = true;
+            endPoint = transform.position + new Vector3(TileSize.x, 0, 0);
+            StartCoroutine(Move());
+        }
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //isMoving = true;
+            endPoint = transform.position + new Vector3(-TileSize.x, 0, 0);
+            StartCoroutine(Move());
+        }
         
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //isMoving = true;
+            endPoint = transform.position + new Vector3(0, TileSize.y, 0);
+            StartCoroutine(Move());
+        }
+
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //isMoving = true;
+            endPoint = transform.position + new Vector3(0, -TileSize.y, 0);
+            StartCoroutine(Move());
+        }
+
+    }
+
+    IEnumerator Move()
+    {
+        if(isMoving)
+        {
+            yield break;
+        }
+
+        isMoving = true;
+        Vector3 startPoint = transform.position;
+        while (MoveToPoint(startPoint)) { yield return null; }
+        
+
+
+        isMoving = false;
+    }
+    bool MoveToPoint(Vector3 StartPoint)
+    {
+        return endPoint != (transform.position = Vector3.MoveTowards(transform.position, endPoint, moveSpeed * Time.deltaTime));
     }
 }
